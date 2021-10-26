39d3ce4391e992065fb592177830e8f2a93cc6b8
palza4dev <moonsj6940@gmail.com>
2021-10-26 11:24:10 +0900

    Add: 카카오 회원가입 및 로그인 기능 구현 - kakao.py에 kakao API class 작성하여 분리하여 관리 - views.py에 KakaoSignUpView class 작 - unittest 완료 - 컨벤션 수정 및 indentation 에러 해결 - 선택동의정보 if문을 없애고 get_or_create 내부로 이동

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+import json, jwt
+from unittest.mock         import MagicMock, patch
 
-# Create your tests here.
+from django.test           import TestCase, Client
+
+from users.models          import User
+from nosleepplace.settings import SECRET_KEY
+
+class KakaoSignUpTest(TestCase):
+    @patch("users.kakao.requests")
+    def test_kakao_signup_new_user_success(self, mocked_requests):
+        client = Client()
+        
+        class MockedResponse:
+            def __init__(self, status_code):
+                self.status_code = status_code
+    
+            def json(self):
+                return {
+                    "id" : "1204",
+                    "kakao_account" : {
+                        "profile" : {
+                            "nickname" : "승준",
+                            "thumbnail_image_url" : "http://k.kakaocdn.net/img_110x110.jpg"
+                        },
+                        "email_needs_agreement" : False,
+                        "email" : "dddsfa@sample.com",
+                        "age_range_needs_agreement" : False,
+                        "age_range" : "20-30"
+                    }
+                }
+
+        mocked_requests.post = MagicMock(return_value = MockedResponse(200))
+        headers              = {"Authorization" : "fake_kakao_token"}
+        response             = client.post("/users/account/kakao", **headers)
+        self.assertEqual(response.status_code, 200)
+
+        access_token = response.json()['access_token']
+        payload      = jwt.decode(access_token, SECRET_KEY, algorithms="HS256")
+        kakao_id     = User.objects.get(id=payload['id']).kakao_id
+        
+        self.assertEqual(kakao_id, "1204")
+
+    @patch("users.kakao.requests")
+    def test_kakao_signup_fail(self, mocked_requests):
+        client = Client()
+        
+        class MockedResponse:
+            def __init__(self,status_code):
+                self.status_code = status_code
+            
+            def json(self):
+                return {
+                    "status_code" : -401,
+                    "message" : "INVALID_KAKAO_TOKEN"
+                }
+                
+        mocked_requests.post = MagicMock(return_value = MockedResponse(200))
+        headers              = {"Authorization" : "fake_kakao_token"}
+        response             = client.post("/users/account/kakao", **headers)
+        
+        self.assertEqual(response.status_code, 400)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import json, requests, jwt
 
-# Create your views here.
+from django.http           import JsonResponse
+from django.views          import View
+
+from nosleepplace.settings import SECRET_KEY
+from users.models          import User
+from users.kakao           import KakaoAPI
+
+class KakaoSignUpView(View):
+    def post(self, request):
+        try:
+            kakao_token   = request.headers.get('Authorization')
+            kakao_user    = KakaoAPI(kakao_token).get_kakao_user()
+            kakao_account = kakao_user['kakao_account']
+
+            nickname      = kakao_account['profile']['nickname'],
+            profile_image = kakao_account['profile']['thumbnail_image_url']
+
+            user, created = User.objects.get_or_create(
+                kakao_id = kakao_user['id'],
+                defaults = {
+                    'nickname'     : nickname,
+                    'profile_image': profile_image,
+                    'email'        : kakao_account.get('email', ''),
+                    'age_range'    : kakao_account.get('age_range', '')
+                }
+            )
+
+            access_token = jwt.encode({'id':user.id}, SECRET_KEY, algorithm='HS256')
+            return JsonResponse({'access_token':access_token}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message':"KEY_ERROR"}, status=400)
+        except ValueError:
+             return JsonResponse({'message':"VALUE_ERROR"}, status=400)
